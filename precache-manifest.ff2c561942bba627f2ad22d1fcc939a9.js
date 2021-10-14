482bffdf2336cc593b14f40e81adc40c319d0f51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-14 01:38:54 +0000

    Deploy vzvvzzv/COMP3104 to github.com/vzvvzzv/COMP3104.git:gh-pages

---
+++ precache-manifest.ff2c561942bba627f2ad22d1fcc939a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "335d37a837d99831a70d",
+    "url": "/vzvvzzv/COMP3104/static/css/main.0ba33d1b.chunk.css"
+  },
+  {
+    "revision": "335d37a837d99831a70d",
+    "url": "/vzvvzzv/COMP3104/static/js/main.335d37a8.chunk.js"
+  },
+  {
+    "revision": "ac2aa791c3f88659fbd8",
+    "url": "/vzvvzzv/COMP3104/static/js/runtime~main.ac2aa791.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/vzvvzzv/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "40549ccf15fd5bbffbc5b1c5f317594e",
+    "url": "/vzvvzzv/COMP3104/index.html"
+  }
+];
